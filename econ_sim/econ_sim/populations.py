85babbfeea4445ac58fda0d23b666b29f17db6ef
ccc <cliffclive@hotmail.com>
2014-01-28 17:11:29 -0600

    Added comments to show changes to populations.py compared to Norvig's original version.

--- econ_sim/econ_sim/populations.py
+++ econ_sim/econ_sim/populations.py
@@ -53,31 +53,45 @@ def random_agent(mu_e1=mu, mu_e2=mu, sigma_e1=mu/3, sigma_e2=mu/3,
 
 class agent(object):
     def __init__(self, endowment1, endowment2, preference1, preference2):
+        '''
+        Agents are characterized by their allocations and preferences
+        of goods 1 and 2. Economists usually call the starting value of
+        a good an "endowment".
+        The variables preference1 and preference2 should be between 0 and 1.
+        '''
         self.good1 = max(0, endowment1)
         self.good2 = max(0, endowment2)
         self.pref1 = preference1
         self.pref2 = preference2
         
     @property
-    def utility(self): return pow(self.good1, self.pref1) * pow(self.good2, self.pref2)
-    
+    def utility(self):
+        '''
+        We'll use the Cobb-Douglas utility function for our model.
+        '''
+        return pow(self.good1, self.pref1) * pow(self.good2, self.pref2)
+
+    # I have a sweet tooth for syntactic sugar.
     @property
-    def allocation(self): return (self.good1, self.good2)
+    def allocation(self):
+        return (self.good1, self.good2)
     
     @allocation.setter
     def allocation(self, values):
         self.good1 = values[0]
         self.good2 = values[1]
-        if self.good1 < 0: print "We've gone negative!"
-        if self.good2 < 0: print "We've gone negative!"
-    
+
+    # We need to make comparisons in order to sort the agents based on utility.
+    # I always prefer to define all of them if I need to define any.
     def __gt__(self, other): return self.utility > other
     def __lt__(self, other): return self.utility < other
     def __eq__(self, other): return self.utility == other
     def __ge__(self, other): return self.utility >= other
     def __le__(self, other): return self.utility <= other
     def __ne__(self, other): return self.utility != other
-    
+
+    # We need to define multiplication and addition operators
+    # in order to use Norvig's normalize function.
     def __mul__(self, other):
         self.good1 *= other
         self.good2 *= other
@@ -85,7 +99,3 @@ class agent(object):
     
     def __radd__(self, other):
         return other + self.good1 + self.good2
-        
-
-
-        
